a1b1d5d196d103af352428d9a58a3be2c3382c6d
dimitri guerroue <dimitri.guerroue@gmail.com>
2019-02-11 18:48:58 +0100

    comment js file

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,10 @@
 window.onload = function (e) { //when images are loaded
   /* nosteal */
 
-  //Styles
-  var nosteal_css = `div.nosteal{ position: relative; }
+  //Styles de la div de protection
+  var nosteal_css = `
+  /* Style nosteal */
+  div.nosteal{ position: relative; }
 	div.nosteal:before{
 		content: '';
 		display: block;
@@ -18,16 +20,19 @@ window.onload = function (e) { //when images are loaded
 		background-color: rgba(0,0,0,0.001);
 	}`;
 
+  //Creation de la balise style dans le head
   var nosteal_style = document.createElement('style');
   nosteal_style.type = 'text/css';
 
+  //Ajout du css dans la balise style
   nosteal_style.appendChild(document.createTextNode(nosteal_css));
   document.head.appendChild(nosteal_style);
 
-  //images
+  //get images
   var nosteal_img_class = 'nostealplease'
   var nosteal_img_nodes = document.querySelectorAll('img.' + nosteal_img_class);
 
+  //Pour chaques images ayant la classe nosteal_img_class y ajouter la div de protection
   nosteal_img_nodes.forEach(function (elem) {
     elem.insertAdjacentHTML('afterend', '<div class="nosteal"></div>');
     elem.nextSibling.appendChild(elem);
